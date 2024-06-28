0cad1ac7c6b2dfe9577a83c9c403d3c9ca4f404a
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-06-28 20:22:25 +0530

    updates priority for create

--- camp/urls.py
+++ camp/urls.py
@@ -4,9 +4,9 @@ from .views import CampaignListAPIView, CampaignDetailAPIView, SubscribeToCampai
 
 urlpatterns = [
     path('campaigns/', CampaignListAPIView.as_view(), name='campaigns'),
+    path('campaigns/create/', CampaignCreateAPIView.as_view(),name='create'),
     path('campaigns/<str:slug>/', CampaignDetailAPIView.as_view()),
     path('subscribe/', SubscribeToCampaignAPIView.as_view(), name='subscribe'),
     path('campaigns/update/<slug:slug>/', CampaignDetailUpdateAPIView.as_view()),
-    path('campaigns/create/', CampaignCreateAPIView.as_view(),name='create'),
     path('campaigns/<slug:slug>/subscribers/', CampaignSubscriberListAPIView.as_view()),
 ]
