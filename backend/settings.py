1c3c0f4b10076198ea8920060c65fbb8c5c67220
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-06-28 14:35:08 +0530

    added static files

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "campaign-app-production.up.railway.app",
+    "https://campaign-app-production.up.railway.app",
 
 ]
 
@@ -163,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
